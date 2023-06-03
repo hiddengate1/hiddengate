1ea5ad09087aa1d35a486e9ad86d3ee6beeaa4af
hiddengate1 <vhhuy1999@gmail.com>
2023-06-03 18:39:25 +0700

    init

---
+++ SubNhanh/src/main/kotlin/com/hiddengate1/SnPlugin.kt
@@ -0,0 +1,13 @@
+package com.hiddengate1
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class SnPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(SnProvider())
+    }
+}
